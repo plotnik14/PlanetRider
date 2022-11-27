6dba300724cdb2b3c52f2e7e53c0db87fcafb37e
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-11-27 23:10:32 +0300

    Falling meteors

---
+++ Assets/Scripts/PlanetRider/Components/EnterCollisionComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace PlanetRider.Components
+{
+    public class EnterCollisionComponent : MonoBehaviour
+    {
+        [SerializeField] private LayerMask _layerMask = ~0;
+        [SerializeField] private UnityEvent _onTrigger;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (IsTouchingLayer(collision.gameObject.layer, _layerMask))
+                _onTrigger?.Invoke();
+        }
+
+        private bool IsTouchingLayer(int objectLayer, LayerMask mask)
+        {
+            return mask == (mask | 1 << objectLayer);
+        }
+    }
+}
--- Assets/Scripts/PlanetRider/Planet.cs
+++ Assets/Scripts/PlanetRider/Planet.cs
@@ -6,15 +6,27 @@ namespace PlanetRider
 {
     public class Planet : MonoBehaviour
     {
+        [Header("Coins")]
         [SerializeField] private ObjectSpawnerComponent _coinSpawner;
-        [SerializeField] private Cooldown _spawnCooldown;
+        [SerializeField] private Cooldown _coinSpawnCooldown;
+        
+        [Header("Meteors")]
+        [SerializeField] private ObjectSpawnerComponent _meteorSpawner;
+        [SerializeField] private Cooldown _meteorSpawnCooldown;
 
         private void Update()
         {
-            if (_spawnCooldown.IsReady)
+            if (_coinSpawnCooldown.IsReady)
             {
                 _coinSpawner.Spawn();
-                _spawnCooldown.Reset();
+                _coinSpawnCooldown.Reset();
+            }
+            
+            
+            if (_meteorSpawnCooldown.IsReady)
+            {
+                _meteorSpawner.Spawn();
+                _meteorSpawnCooldown.Reset();
             }
         }
     }
