056fb9ecd4a2664e0b18e67458de06e35f30fd83
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-12-07 20:15:48 +0300

    Added more trees + rotation component

---
+++ Assets/Scripts/PlanetRider/Components/EditorRotateToTargetComponent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace PlanetRider.Components
+{
+    public class EditorRotateToTargetComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+
+        [ContextMenu("Rotate")]
+        private void Rotate()
+        {
+            transform.rotation = Quaternion.identity;
+            var direction = _target.position - transform.position;
+            transform.rotation = Quaternion.FromToRotation( - transform.up, direction);
+        }
+        
+        private void OnDrawGizmosSelected()
+        {
+            var direction = _target.position - transform.position;
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawRay(transform.position, direction);
+
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawRay(transform.position, - transform.up);
+            
+            Gizmos.color = Color.white;
+        }
+    }
+}
